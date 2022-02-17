0981560af80b0cb152ce8b6250cc846371b950ef
ReneRambow <bill12gate@gmail.com>
2022-02-17 13:43:24 +0100

    'M

--- list-rekenen.py
+++ list-rekenen.py
@@ -1,26 +1,31 @@
 listOne = [i for i in range(1, 11)]
 listTwo = [i for i in range(2, 21, 2)]
 
-def addAndDisplayLists(l1, l2):
+def addAndDisplayLists(listOne, listTwo): 
     print('---------')
     print('Add lists')
-    for i in range(min(len(l1), len(l2))):
-        print("{:<2} + {:<2} = {:<5}".format(l1[i], l2[i], l1[i] + l2[i]))
+    for i in range(min(len(listOne), len(listTwo))):
+        print("{:<2} + {:<2} = {:<5}".format(listOne[i], listTwo[i], listOne[i] + listTwo[i]))
 
-def substractAndDisplayLists(l1, l2):
+def substractAndDisplayLists(listOne, listTwo):
     print('---------')
     print('Substract lists')
-    for i in range(min(len(l1), len(l2))):
-        print("{:<2} + {:<2} = {:<5}".format(l1[i], l2[i], l1[i] + l2[i]))
+    for i in range(min(len(listOne), len(listTwo))):
+        print("{:<2} - {:<2} = {:<5}".format(listOne[i], listTwo[i], listOne[i] - listTwo[i]))
 
-def multiplyAndDisplayLists (l1, l2):
+def multiplyAndDisplayLists (listOne, listTwo):
     print('---------')
     print('Multiply lists')
-    for i in range(min(len(l1), len(l2))):
-        print("{:<2} + {:<2} = {:<5}".format(l1[i], l2[i], l1[i] + l2[i]))
+    for i in range(min(len(listOne), len(listTwo))):
+        print("{:<2} * {:<2} = {:<5}".format(listOne[i], listTwo[i], listOne[i] * listTwo[i]))
 
-def divideAndDisplayLists(l1, l2):
+def divideAndDisplayLists(listOne, listTwo):
     print('---------')
     print('Divide lists')
-    for i in range(min(len(l1), len(l2))):
-        print("{:<2} + {:<2} = {:<5}".format(l1[i], l2[i], l1[i] + l2[i]))
+    for i in range(min(len(listOne), len(listTwo))):
+        print("{:<2} / {:<2} = {:<5}".format(listOne[i], listTwo[i], listOne[i] / listTwo[i]))
+
+addAndDisplayLists(listOne, listTwo)
+substractAndDisplayLists(listOne, listTwo)
+multiplyAndDisplayLists(listOne, listTwo)
+divideAndDisplayLists(listOne, listTwo)
